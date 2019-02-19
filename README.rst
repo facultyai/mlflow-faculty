5cd76daa379322e28b7ff7e676dc14869402c129
Andrew Crozier <wacrozier@gmail.com>
2019-02-19 11:08:51 +0000

    Initial commit with package framework

---
+++ README.rst
@@ -0,0 +1,4 @@
+mlflow-faculty
+==============
+
+A plugin for using MLflow with the Faculty platform.
---
+++ setup.py
@@ -0,0 +1,30 @@
+# Copyright 2019 Faculty Science Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="mlflow-faculty",
+    description="MLflow plugin for the Faculty platform.",
+    url="https://faculty.ai/",
+    author="Faculty",
+    author_email="opensource@faculty.ai",
+    license="Apache Software License",
+    packages=find_packages(),
+    use_scm_version={"version_scheme": "post-release"},
+    setup_requires=["setuptools_scm"],
+    install_requires=["faculty", "mlflow"],
+)
