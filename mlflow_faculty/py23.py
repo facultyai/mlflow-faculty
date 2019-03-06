2bc5ea44c77569d7eab501d2e6f26c68e56fbf7c
Pascal Bugnion <pascal@bugnion.org>
2019-03-06 14:39:05 +0000

    flake8 fixes

--- mlflow_faculty/py23.py
+++ mlflow_faculty/py23.py
@@ -4,9 +4,9 @@ from datetime import datetime
 
 EPOCH = datetime(1970, 1, 1, tzinfo=UTC)
 
+
 def to_timestamp(dt):
     if six.PY2:
         return (dt - EPOCH).total_seconds()
     else:
         return dt.timestamp()
-
