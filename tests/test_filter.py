ee2404a710dd4cf2dd57c6b50b05ddfeda340c9d
Andrew Crozier <wacrozier@gmail.com>
2019-08-22 15:58:42 +0100

    Additional testing

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -82,7 +82,7 @@ def _build_test_cases(builder, identifier, operator_cases, value_cases):
 
 def _run_id_test_cases():
     for type_identifier in ATTRIBUTE_IDENTIFIERS:
-        for sql_key in ["id", "run_id", "runId", '"id"', "`run_id`"]:
+        for sql_key in ["id", "run_id", '"id"', "`run_id`"]:
             identifier = "{}.{}".format(type_identifier, sql_key)
             yield from _build_defined_test_cases(RunIdFilter, identifier)
             yield from _build_test_cases(
@@ -147,7 +147,132 @@ def _single_test_cases():
 @pytest.mark.parametrize(
     "filter_string, expected_filter", _single_test_cases()
 )
-def test_single(filter_string, expected_filter):
+def test_parse_filter_string_single(filter_string, expected_filter):
+    filter = parse_filter_string(filter_string)
+    assert filter == expected_filter
+    assert isinstance(filter, type(expected_filter))
+
+
+RUN_ID_FILTER_STRING = 'attribute.run_id = "{}"'.format(RUN_ID)
+RUN_ID_FILTER = RunIdFilter(ComparisonOperator.EQUAL_TO, RUN_ID)
+
+STATUS_FILTER_STRING = 'run.status != "FINISHED"'
+STATUS_FILTER = RunStatusFilter(
+    ComparisonOperator.NOT_EQUAL_TO, ExperimentRunStatus.FINISHED
+)
+
+PARAM_FILTER_STRING = "param.alpha > 20"
+PARAM_FILTER = ParamFilter("alpha", ComparisonOperator.GREATER_THAN, 20)
+
+METRIC_FILTER_STRING = "metric.accuracy IS NOT NULL"
+METRIC_FILTER = MetricFilter("accuracy", ComparisonOperator.DEFINED, True)
+
+TAG_FILTER_STRING = "tag.`class.name` IS NULL"
+TAG_FILTER = TagFilter("class.name", ComparisonOperator.DEFINED, False)
+
+REDUCED_SINGLE_TEST_CASES = [
+    (RUN_ID_FILTER_STRING, RUN_ID_FILTER),
+    (STATUS_FILTER_STRING, STATUS_FILTER),
+    (PARAM_FILTER_STRING, PARAM_FILTER),
+    (METRIC_FILTER_STRING, METRIC_FILTER),
+    (TAG_FILTER_STRING, TAG_FILTER),
+]
+
+
+@pytest.mark.parametrize(
+    "sql_operator, expected_operator",
+    [
+        ("AND", LogicalOperator.AND),
+        ("and", LogicalOperator.AND),
+        ("OR", LogicalOperator.OR),
+        ("or", LogicalOperator.OR),
+    ],
+)
+@pytest.mark.parametrize("left_string, left_filter", REDUCED_SINGLE_TEST_CASES)
+@pytest.mark.parametrize(
+    "right_string, right_filter", REDUCED_SINGLE_TEST_CASES
+)
+def test_parse_filter_string_logical_operator(
+    sql_operator,
+    expected_operator,
+    left_string,
+    left_filter,
+    right_string,
+    right_filter,
+):
+    filter_string = "{} {} {}".format(left_string, sql_operator, right_string)
+    expected_filter = CompoundFilter(
+        expected_operator, [left_filter, right_filter]
+    )
+    filter = parse_filter_string(filter_string)
+    assert filter == expected_filter
+    assert isinstance(filter, type(expected_filter))
+
+
+OPERATOR_PRECEDENCE_FILTER_STRING = "{} AND {} OR {}".format(
+    RUN_ID_FILTER_STRING, STATUS_FILTER_STRING, METRIC_FILTER_STRING
+)
+OPERATOR_PRECEDENCE_FILTER = CompoundFilter(
+    LogicalOperator.OR,
+    [
+        CompoundFilter(LogicalOperator.AND, [RUN_ID_FILTER, STATUS_FILTER]),
+        METRIC_FILTER,
+    ],
+)
+
+
+def test_parse_filter_string_operator_precedence():
+    filter = parse_filter_string(OPERATOR_PRECEDENCE_FILTER_STRING)
+    assert filter == OPERATOR_PRECEDENCE_FILTER
+    assert isinstance(filter, type(OPERATOR_PRECEDENCE_FILTER))
+
+
+PAREN_FILTER_STRING = "{} AND ({} OR {}) ".format(
+    RUN_ID_FILTER_STRING, STATUS_FILTER_STRING, METRIC_FILTER_STRING
+)
+PAREN_FILTER = CompoundFilter(
+    LogicalOperator.AND,
+    [
+        RUN_ID_FILTER,
+        CompoundFilter(LogicalOperator.OR, [STATUS_FILTER, METRIC_FILTER]),
+    ],
+)
+
+NESTED_PAREN_FILTER_STRING = "({} OR {} OR ({} AND {})) AND {}".format(
+    RUN_ID_FILTER_STRING,
+    STATUS_FILTER_STRING,
+    PARAM_FILTER_STRING,
+    METRIC_FILTER_STRING,
+    TAG_FILTER_STRING,
+)
+NESTED_PAREN_FILTER = CompoundFilter(
+    LogicalOperator.AND,
+    [
+        CompoundFilter(
+            LogicalOperator.OR,
+            [
+                RUN_ID_FILTER,
+                STATUS_FILTER,
+                CompoundFilter(
+                    LogicalOperator.AND, [PARAM_FILTER, METRIC_FILTER]
+                ),
+            ],
+        ),
+        TAG_FILTER,
+    ],
+)
+
+
+@pytest.mark.parametrize(
+    "filter_string, expected_filter",
+    [
+        ("({})".format(RUN_ID_FILTER_STRING), RUN_ID_FILTER),
+        (" ( {} ) ".format(STATUS_FILTER_STRING), STATUS_FILTER),
+        (PAREN_FILTER_STRING, PAREN_FILTER),
+        (NESTED_PAREN_FILTER_STRING, NESTED_PAREN_FILTER),
+    ],
+)
+def test_parse_filter_string_parentheses(filter_string, expected_filter):
     filter = parse_filter_string(filter_string)
     assert filter == expected_filter
     assert isinstance(filter, type(expected_filter))
