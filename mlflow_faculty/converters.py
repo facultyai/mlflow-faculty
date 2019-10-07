3fe0a36f904ee9ec1fc1395713e2774c003b6957
Andrew Crozier <wacrozier@gmail.com>
2019-10-07 16:01:53 +0100

    Convert experiment IDs to strings for MLflow

--- mlflow_faculty/converters.py
+++ mlflow_faculty/converters.py
@@ -80,7 +80,7 @@ def _datetime_to_mlflow_timestamp(dt):
 def faculty_experiment_to_mlflow_experiment(faculty_experiment):
     active = faculty_experiment.deleted_at is None
     return Experiment(
-        faculty_experiment.id,
+        str(faculty_experiment.id),
         faculty_experiment.name,
         faculty_experiment.artifact_location,
         LifecycleStage.ACTIVE if active else LifecycleStage.DELETED,
